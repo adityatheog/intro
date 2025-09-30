2c6972380d6115ec6f41ad5fa0f07aa626bb540e
adityatheog <aaatoxicff07@gmail.com>
2025-09-30 18:14:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ const SITE_CONFIG = {
     // Social links
     socialLinks: {
         github: "https://github.com/adityatheog",
-        instragram : "https://instagram.com/adityatheog.github.io",
+        instragram : "https://instagram.com/_adityatheog",
         youtube: "https://www.youtube.com/@ADiTYaTheOG_YouTube",
         discord: "https://discord.gg/c7QpxGtZMk",
         telegram: "https://t.me/adityatheog"
